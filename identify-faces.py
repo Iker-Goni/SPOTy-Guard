e94897e19e2de2cbfd57058298b63ec40188b1e0
Nick <114621135+NickTriance@users.noreply.github.com>
2024-02-19 15:11:39 -0800

    Now dumps faces into stored-faces folder correctly.

--- identify-faces.py
+++ identify-faces.py
@@ -1,9 +1,16 @@
 import cv2
+import sys
+import os
+import pathlib
 
 cascade_file = "haarcascade_frontalface_default.xml"
 haar_cascade = cv2.CascadeClassifier(cascade_file)
 
-file_name = 'nick.jpg'
+file_name = sys.argv[1] # take in image file name
+
+# create directory for images if it doesn't exist
+pathlib.Path('stored-faces').mkdir(parents=True, exist_ok=True) 
+
 
 img = cv2.imread(file_name, 0)
 
@@ -16,6 +23,6 @@ i = 0
 for x, y, w, h in faces:
 
     cropped_image = img[y : y + h, x : x + w]
-    target_file_name = file_name + '_face' + str(i) + '.jpg'
+    target_file_name = 'stored-faces/' + file_name + '_face' + str(i) + '.jpg'
     cv2.imwrite(target_file_name, cropped_image)
     i += 1
