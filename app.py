074d3bd884f9b8987598be26dd1c94157d8168f3
minnowfeather <matthewparedes2k3@gmail.com>
2024-02-19 17:44:05 -0800

    Buttons + skeleton code for backend integration

--- app.py
+++ app.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+# ToDO: Integrate with SPOTyGuard Backend
+@app.route('/facereg-<status>')
+def facereg(status):
+    if status == "enable":
+        print("waow")
+    elif status == "disable":
+        print("waow 2")
+    return redirect("/")
+# ToDO: Integrate with SPOTyGuard Backend
+@app.route("/patrol-<status>")
+def patrol(status):
+    if status == "enable":
+        print("wowowo")
+    elif status == "disable":
+        print("wowowo 2")
+    return redirect("/")
+
+
+
 if __name__ == '__main__':
     app.run(debug=true)
